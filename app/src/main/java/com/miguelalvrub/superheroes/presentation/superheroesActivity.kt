b9a38abc96c40dcb57ac6b6da75ba6793324b52f
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2022-10-18 16:46:47 +0200

    Start Project

---
+++ app/src/main/java/com/miguelalvrub/superheroes/presentation/superheroesActivity.kt
@@ -0,0 +1,12 @@
+package com.miguelalvrub.superheroes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miguelalvrub.superheroes.R
+
+class superheroesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_superheroes)
+    }
+}
